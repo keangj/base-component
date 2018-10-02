b040e17dd6f5f77da83806505d72ea2e15bf2cd6
jay <keangj@outlook.com>
2018-10-02 17:52:04 +0800

    添加button基本样式

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import  Button from './button'
+
+Vue.component('b-button', Button)
+
+new Vue({
+  el: '#app'
+})
---
+++ src/button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="b-button">按钮</button>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.b-button {
+  border: 1px solid var(--border-color);
+  border-radius: var(--border-radius);
+  padding: 0 1em;
+  font-size: var(--font-size);
+  height: var(--button-height);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--border-color);
+  }
+  &:active {
+  background-color: var(--button-active-bg);
+  }
+  &:focus {
+     outline: none;
+  }
+}
+</style>
