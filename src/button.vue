914804a60a44b082bd6207625eebeed569c3045f
jay <keangj@outlook.com>
2018-10-03 16:06:45 +0800

    添加icon组件

--- src/button.vue
+++ src/button.vue
@@ -1,33 +1,64 @@
 <template>
-  <button class="b-button">按钮</button>
+  <button class="b-button" :class="{[`icon-${iconPosition}`]: true}">
+    <b-icon class="icon" :name="icon"></b-icon>
+    <div class="b-button-content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-
+  export default {
+    props: {
+      icon: {},
+      iconPosition: {
+        default: 'left'
+      }
     }
   }
-}
 </script>
 
-<style lang="scss">
-.b-button {
-  border: 1px solid var(--border-color);
-  border-radius: var(--border-radius);
-  padding: 0 1em;
-  font-size: var(--font-size);
-  height: var(--button-height);
-  background: var(--button-bg);
-  &:hover {
-    border-color: var(--border-color);
+<style lang="scss" type="text/scss" scoped>
+  .b-button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid var(--border-color);
+    border-radius: var(--border-radius);
+    padding: 0 1.2em;
+    font-size: var(--font-size);
+    height: var(--button-height);
+    line-height: var(--button-height);
+    background: var(--button-bg);
+    vertical-align: middle;
+    &:hover {
+      border-color: var(--border-color);
+    }
+
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+
+    &:focus {
+      outline: none;
+    }
+    > .b-button-content {
+      order: 2;
+    }
+    > .icon {
+      order: 1;
+      margin-right: .2em;
+    }
+
+    &.icon-right {
+      > .b-button-content {
+        order: 1;
+      }
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .2em;
+      }
+    }
   }
-  &:active {
-  background-color: var(--button-active-bg);
-  }
-  &:focus {
-     outline: none;
-  }
-}
 </style>
---
+++ src/icon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg class="b-icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style lang="scss" type="text/scss" scoped>
+  .b-icon {
+    width: 1em;
+    height: 1em;
+  }
+</style>
