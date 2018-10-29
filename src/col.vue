251deddac2b51ee18473f5b708c807132dbc83c0
jay <keangj@outlook.com>
2018-10-29 16:54:02 +0800

    网格布局 修复了添加 class 出错的问题

--- src/col.vue
+++ src/col.vue
@@ -47,15 +47,15 @@
         let {span, offset, xs, sm, md, lg, xl, xxl} = this
         let createClasses = this.createClasses
         return [
-          span && `col-${span}`,
-          offset && `offset-${offset}`,
-          ...createClasses({ span, offset}),
-          ...createClasses(xs, 'xs'),
-          ...createClasses(md, 'md'),
-          ...createClasses(sm, 'sm'),
-          ...createClasses(lg, 'lg'),
-          ...createClasses(xl, 'xl'),
-          ...createClasses(xxl, 'xxl')
+          // span && `col-${span}`,
+          // offset && `offset-${offset}`,
+          ...createClasses({ span, offset }),
+          ...createClasses(xs, 'xs-'),
+          ...createClasses(md, 'md-'),
+          ...createClasses(sm, 'sm-'),
+          ...createClasses(lg, 'lg-'),
+          ...createClasses(xl, 'xl-'),
+          ...createClasses(xxl, 'xxl-')
         ]
       },
       colStyle () {
@@ -70,8 +70,8 @@
       createClasses (obj, str = '') {
         if (!obj) { return [] }
         let arr = []
-        if (obj.span) {arr.push(`col-${str}-${obj.span}`)}
-        if (obj.offset) {arr.push(`offset-${str}-${obj.offset}`)}
+        if (obj.span) {arr.push(`col-${str}${obj.span}`)}
+        if (obj.offset) {arr.push(`offset-${str}${obj.offset}`)}
         return arr
       }
     }
@@ -82,13 +82,13 @@
 .col {
   // scss for 循环
   $class-prefix: col-;
-  @for $n from 1 through 24 {
+  @for $n from 0 through 24 {
     &.#{$class-prefix}#{$n} {
       width: $n / 24 * 100%;
     }
   }
   $class-prefix: offset-;
-  @for $n from 1 through 24 {
+  @for $n from 0 through 24 {
     &.#{$class-prefix}#{$n} {
       margin-left: $n / 24 * 100%;
     }
@@ -96,73 +96,73 @@
   
   @media (max-width: 576px) {
     $class-prefix: col-xs-;
-    @for $n from 1 through 24 {
+    @for $n from 0 through 24 {
       &.#{$class-prefix}#{$n} {
         width: $n / 24 * 100%;
       }
     }
     $class-prefix: offset-xs-;
-    @for $n from 1 through 24 {
+    @for $n from 0 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: $n / 24 * 100%;
       }
     }
   }
 
-  @media (min-width: 577px) {
+  @media (min-width: 576px) {
     $class-prefix: col-sm-;
-    @for $n from 1 through 24 {
+    @for $n from 0 through 24 {
       &.#{$class-prefix}#{$n} {
         width: $n / 24 * 100%;
       }
     }
     $class-prefix: offset-sm-;
-    @for $n from 1 through 24 {
+    @for $n from 0 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: $n / 24 * 100%;
       }
     }
   }
 
-  @media (min-width: 769px) {
+  @media (min-width: 768px) {
     $class-prefix: col-md-;
-    @for $n from 1 through 24 {
+    @for $n from 0 through 24 {
       &.#{$class-prefix}#{$n} {
         width: $n / 24 * 100%;
       }
     }
     $class-prefix: offset-md-;
-    @for $n from 1 through 24 {
+    @for $n from 0 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: $n / 24 * 100%;
       }
     }
   }
 
-  @media (min-width: 993px) {
+  @media (min-width: 992px) {
     $class-prefix: col-lg-;
-    @for $n from 1 through 24 {
+    @for $n from 0 through 24 {
       &.#{$class-prefix}#{$n} {
         width: $n / 24 * 100%;
       }
     }
     $class-prefix: offset-lg-;
-    @for $n from 1 through 24 {
+    @for $n from 0 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: $n / 24 * 100%;
       }
     }
   }
 
-  @media (min-width: 1201px) {
+  @media (min-width: 1200px) {
     $class-prefix: col-xl-;
-    @for $n from 1 through 24 {
+    @for $n from 0 through 24 {
       &.#{$class-prefix}#{$n} {
         width: $n / 24 * 100%;
       }
     }
     $class-prefix: offset-xl-;
-    @for $n from 1 through 24 {
+    @for $n from 0 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: $n / 24 * 100%;
       }
@@ -171,13 +171,13 @@
 
   @media (min-width: 1600px) {
     $class-prefix: col-xxl-;
-    @for $n from 1 through 24 {
+    @for $n from 0 through 24 {
       &.#{$class-prefix}#{$n} {
         width: $n / 24 * 100%;
       }
     }
     $class-prefix: offset-xxl-;
-    @for $n from 1 through 24 {
+    @for $n from 0 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: $n / 24 * 100%;
       }
