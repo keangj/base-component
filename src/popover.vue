91795884c15e973552db4055b59b70b47052a6f4
kjay <keangjay@gmail.com>
2018-12-09 23:51:10 +0800

    refactor popover 组件重构。

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="popover" @click="onClick">
+  <div class="popover" @click="onClick" ref="popover">
     <div class="content-wrapper" ref="contentWrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
@@ -18,26 +18,42 @@
       }
     },
     methods: {
+      positionContent () {
+        // 将 popover 内容区域移动到 dom 最外层
+        document.body.appendChild(this.$refs.contentWrapper)
+        // 设置内容区域的位置
+        let { left, top } = this.$refs.triggerWrapper.getBoundingClientRect()
+        this.$refs.contentWrapper.style.top = `${window.scrollY + top}px`
+        this.$refs.contentWrapper.style.left = `${window.scrollY + left}px`
+      },
+      elementHandler (event) {
+        if (this.$refs.popover &&
+          (this.$refs.popover === event.target || this.$refs.popover.contains(event.target))
+        ) {return}
+        if (this.$refs.contentWrapper &&
+          (this.$refs.contentWrapper === event.target || this.$refs.contentWrapper.contains(event.target))
+        ) {return}
+        this.close()
+      },
+      close () {
+        this.visible = false
+        console.log('移除监听')
+        document.removeEventListener('click', this.elementHandler)
+      },
+      open () {
+        this.visible = true
+        this.$nextTick(() => {
+          this.positionContent()
+          console.log('添加监听')
+          document.addEventListener('click', this.elementHandler)
+        })
+      },
       onClick (event) {
         if (this.$refs.triggerWrapper.contains(event.target)) {
-          this.visible = !this.visible
           if (this.visible) {
-            this.$nextTick(() => {
-              // 将 popover 内容区域移动到 dom 最外层
-              document.body.appendChild(this.$refs.contentWrapper)
-              // 设置内容区域的位置
-              let {left, top} = this.$refs.triggerWrapper.getBoundingClientRect()
-              this.$refs.contentWrapper.style.top = `${window.scrollY + top}px`
-              this.$refs.contentWrapper.style.left = `${window.scrollY + left}px`
-              let elementHandler = (event) => {
-                if (!this.$refs.contentWrapper.contains(event.target)) {
-                  this.visible = false
-                  document.removeEventListener('click', elementHandler)
-                }
-              }
-              console.log('添加监听')
-              document.addEventListener('click', elementHandler)
-            })
+            this.close()
+          } else {
+            this.open()
           }
         }
       }
