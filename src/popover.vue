cce7a9eda0ddcfb9851b565a31c09b161faa304d
kjay <keangjay@gmail.com>
2018-12-10 22:45:50 +0800

    feat popover 组件，添加 hover 触发方式。

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="popover" @click="onClick" ref="popover">
+  <div class="popover" ref="popover">
     <div
       class="content-wrapper"
       ref="contentWrapper"
@@ -24,11 +24,34 @@
         validator (value) {
           return ['top', 'bottom', 'left', 'right'].indexOf(value) !== -1
         }
+      },
+      trigger: {
+        type: String,
+        default: 'click',
+        validator (value) {
+          return ['click', 'hover'].indexOf(value) !== -1
+        }
       }
     },
     data () {
       return {
         visible: false
+      }
+    },
+    mounted () {
+      if (this.trigger === 'click') {
+        this.$refs.popover.addEventListener('click', this.onClick)
+      } else {
+        this.$refs.popover.addEventListener('mouseenter', this.open)
+        this.$refs.popover.addEventListener('mouseleave', this.close)
+      }
+    },
+    destroyed () {
+      if (this.trigger === 'click') {
+        this.$refs.popover.removeEventListener('click', this.onClick)
+      } else {
+        this.$refs.popover.removeEventListener('mouseenter', this.open)
+        this.$refs.popover.removeEventListener('mouseleave', this.close)
       }
     },
     methods: {
