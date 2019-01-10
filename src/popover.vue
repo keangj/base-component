e96d8f9625a8a1e36fd24a145b91bc686f8c23c1
kjay <keangjay@gmail.com>
2019-01-10 22:07:59 +0800

    fix(popover)，修复销毁组件出错问题。

--- src/popover.vue
+++ src/popover.vue
@@ -46,7 +46,7 @@
         this.$refs.popover.addEventListener('mouseleave', this.close)
       }
     },
-    destroyed () {
+    beforeDestroy () {
       if (this.trigger === 'click') {
         this.$refs.popover.removeEventListener('click', this.onClick)
       } else {
