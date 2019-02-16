d8748cc99966683eba6e448ae489c80743dc5d91
kjay <keangjay@gmail.com>
2019-02-17 00:05:06 +0800

    添加 popover 组件的文档

---
+++ docs/.vuepress/components/popover-demo2.vue
@@ -0,0 +1,86 @@
+<template>
+  <div style="padding-top: 16px;">
+    <h2>hover 激活</h2>
+    <p>
+      <strong>预览</strong>
+    </p>
+
+    <b-popover trigger="hover">
+      <b-button>上方</b-button>
+      <template slot="content">
+        内容
+      </template>
+    </b-popover>
+    <b-popover position="bottom" trigger="hover">
+      <b-button>下方</b-button>
+      <template slot="content">
+        内容
+      </template>
+    </b-popover>
+    <b-popover position="left" trigger="hover">
+      <b-button>左边</b-button>
+      <template slot="content">
+        内容
+      </template>
+    </b-popover>
+    <b-popover position="right" trigger="hover">
+      <b-button>右边</b-button>
+      <template slot="content">
+        内容
+      </template>
+    </b-popover>
+
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
+  </div>
+</template>
+<style>
+</style>
+<style scoped>
+</style>
+<script>
+  import BButton from '../../../src/button/button'
+  import BPopover from '../../../src/popover'
+  export default {
+    components: {BButton, BPopover},
+    data () {
+      return {
+        content: `
+          <b-popover>
+            <b-button>上方</b-button>
+            <template slot="content">
+              内容
+            </template>
+          </b-popover>
+          <b-popover position="bottom">
+            <b-button>下方</b-button>
+            <template slot="content">
+              内容
+            </template>
+          </b-popover>
+          <b-popover position="left">
+            <b-button>左边</b-button>
+            <template slot="content">
+              内容
+            </template>
+          </b-popover>
+          <b-popover position="right">
+            <b-button>右边</b-button>
+            <template slot="content">
+              内容
+            </template>
+          </b-popover>
+      `.replace(/^ {8}/gm, '').trim()
+      }
+    },
+    methods: {
+      onClickButton () {
+        this.$toast('<strong style="color:red;">加粗的提示</strong>', {
+          enableHtml: true
+        })
+      }
+    },
+  }
+</script>
---
+++ docs/components/popover.md
@@ -0,0 +1,9 @@
+---
+title: Popover
+sidebarDepth: 2
+---
+# Popover
+<ClientOnly>
+<popover-demo></popover-demo>
+<popover-demo2></popover-demo2>
+</ClientOnly>
