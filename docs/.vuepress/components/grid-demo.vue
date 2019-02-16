ea0f20de5dc636a56fa3a73fba11fe7acea5897a
kjay <keangjay@gmail.com>
2019-02-17 00:04:15 +0800

    添加 grid 组件的文档

---
+++ docs/.vuepress/components/grid-demo.vue
@@ -0,0 +1,217 @@
+<template>
+  <div style="padding-top: 16px;">
+    <h2>基本用法</h2>
+    <p>
+      <strong>预览</strong>
+    </p>
+    <b-row>
+      <b-col span="8">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="8">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="8">
+        <div class="b-content"></div>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col span="6">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="6">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="6">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="6">
+        <div class="b-content"></div>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col span="4">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="4">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="4">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="4">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="4">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="4">
+        <div class="b-content"></div>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col span="2">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="2">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="2">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="2">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="2">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="2">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="2">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="2">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="2">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="2">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="2">
+        <div class="b-content"></div>
+      </b-col>
+      <b-col span="2">
+        <div class="b-content"></div>
+      </b-col>
+    </b-row>
+
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
+  </div>
+</template>
+<style scoped>
+  * {
+    box-sizing: border-box;
+  }
+  .b-row {
+    margin: 10px 0;
+  }
+  .b-content {
+    height: 50px;
+    border: 1px solid #F2F6FC;
+    background: #aaa;
+    border-radius: 4px;
+  }
+</style>
+<script>
+  import bRow from '../../../src/grid/row'
+  import bCol from '../../../src/grid/col'
+  export default {
+    components: {bRow, bCol},
+    data () {
+      return {
+        content: `
+          <b-row>
+            <b-col span="8">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="8">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="8">
+              <div class="b-content"></div>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col span="6">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="6">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="6">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="6">
+              <div class="b-content"></div>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col span="4">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="4">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="4">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="4">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="4">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="4">
+              <div class="b-content"></div>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col span="2">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="2">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="2">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="2">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="2">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="2">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="2">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="2">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="2">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="2">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="2">
+              <div class="b-content"></div>
+            </b-col>
+            <b-col span="2">
+              <div class="b-content"></div>
+            </b-col>
+          </b-row>
+          <style>
+            * {
+              box-sizing: border-box;
+            }
+            .b-content {
+              height: 50px;
+              border: 1px solid #F2F6FC;
+              background: #aaa;
+              border-radius: 4px;
+            }
+          </style>
+      `.replace(/^ {8}/gm, '').trim()
+      }
+    }
+  }
+</script>
---
+++ docs/components/grid.md
@@ -0,0 +1,10 @@
+---
+title: Grid
+sidebarDepth: 2
+---
+# Grid
+<ClientOnly>
+<grid-demo></grid-demo>
+<grid-demo2></grid-demo2>
+<grid-demo3></grid-demo3>
+</ClientOnly>
